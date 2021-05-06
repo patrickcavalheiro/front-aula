d06237be7258187f168662af8b4a12e8623bef1b
Patrick Cavalheiro <pwcavalheiro@gmail.com>
2021-05-05 22:00:19 -0300

    Configurações extras da requisição CURL para deixa-la mais robusta

--- application/libraries/Wsaulaservice.php
+++ application/libraries/Wsaulaservice.php
@@ -17,16 +17,29 @@ class Wsaulaservice {
     public function get($endpoint = '') {
         if ($endpoint != '') {
             try {
+                //seta um timeout
+                $this->default_timeout = ini_get('default_socket_timeout');
+                ini_set('default_socket_timeout', 30); //segundos
+
                 /* realiza a requisição para a API */
                 $this->curl = curl_init();
                 curl_setopt($this->curl, CURLOPT_URL, $this->url_ws . $endpoint);
                 curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
                 curl_setopt($this->curl, CURLOPT_HTTPHEADER, array('token: ' . $this->token));
+                //faz algumas configurações extras da conexão
+                curl_setopt($this->curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+                curl_setopt($this->curl, CURLOPT_REFERER, $_SERVER['SERVER_NAME']);                                
+                curl_setopt($this->curl, CURLOPT_TIMEOUT, 30);
+                curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, true);
+                
                 $retornoApi = curl_exec($this->curl);
                 curl_close($this->curl);
 
                 return json_decode($retornoApi);                
-            } catch (Throwable $t) {                
+            } catch (Throwable $t) {       
+                //retorna o timeout para o seu tempo padrão
+                ini_set('default_socket_timeout', $this->default_timeout);
                 error_clear_last();
                 return false;
             }
@@ -38,6 +51,10 @@ class Wsaulaservice {
     public function post($endpoint = '', $dados = null) {
         if ($endpoint != '' && $dados != null) {
             try {
+                //seta um timeout
+                $this->default_timeout = ini_get('default_socket_timeout');
+                ini_set('default_socket_timeout', 30); //segundos
+
                 /* realiza a requisição para a API */
                 $this->curl = curl_init();
                 curl_setopt($this->curl, CURLOPT_URL, $this->url_ws . $endpoint);
@@ -47,11 +64,20 @@ class Wsaulaservice {
                 curl_setopt($this->curl, CURLOPT_POST, count($dados));
                 curl_setopt($this->curl, CURLOPT_POSTFIELDS, urldecode(http_build_query($dados)));
 
+                //faz algumas configurações extras da conexão
+                curl_setopt($this->curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+                curl_setopt($this->curl, CURLOPT_REFERER, $_SERVER['SERVER_NAME']);                                
+                curl_setopt($this->curl, CURLOPT_TIMEOUT, 30);
+                curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, true);
+
                 $retornoApi = curl_exec($this->curl);
                 curl_close($this->curl);
 
                 return json_decode($retornoApi);
-            } catch (Throwable $t) {                
+            } catch (Throwable $t) {  
+                //retorna o timeout para o seu tempo padrão
+                ini_set('default_socket_timeout', $this->default_timeout);              
                 error_clear_last();
                 return false;
             }
@@ -63,6 +89,10 @@ class Wsaulaservice {
     public function put($endpoint = '', $dados = null) {
         if ($endpoint != '' && $dados != null) {
             try {
+                //seta um timeout
+                $this->default_timeout = ini_get('default_socket_timeout');
+                ini_set('default_socket_timeout', 30); //segundos
+
                 /* realiza a requisição para a API */
                 $this->curl = curl_init();
                 curl_setopt($this->curl, CURLOPT_URL, $this->url_ws . $endpoint);
@@ -72,11 +102,20 @@ class Wsaulaservice {
                 curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, 'PUT');
                 curl_setopt($this->curl, CURLOPT_POSTFIELDS, urldecode(http_build_query($dados)));
 
+                //faz algumas configurações extras da conexão
+                curl_setopt($this->curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+                curl_setopt($this->curl, CURLOPT_REFERER, $_SERVER['SERVER_NAME']);                                
+                curl_setopt($this->curl, CURLOPT_TIMEOUT, 30);
+                curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, true);
+
                 $retornoApi = curl_exec($this->curl);
                 curl_close($this->curl);
 
                 return json_decode($retornoApi);
-            } catch (Throwable $t) {                
+            } catch (Throwable $t) {    
+                //retorna o timeout para o seu tempo padrão
+                ini_set('default_socket_timeout', $this->default_timeout);            
                 error_clear_last();
                 return false;
             }
